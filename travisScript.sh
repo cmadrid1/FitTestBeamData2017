2c8d99f29efff42ca66de67ba3232b2baf2923b1
Christopher_Madrid <Chris_Madrid@baylor.edu>
2018-01-20 14:31:07 +0100

    setting up a cmssw scram arch

--- travisScript.sh
+++ travisScript.sh
@@ -4,9 +4,10 @@ cd $1
 #source /cvmfs/sft.cern.ch/lcg/views/LCG_89/x86_64-slc6-gcc62-opt/setup.sh
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 
+export SCRAM_ARCH=slc6_amd64_gcc630
 scramv1 project CMSSW CMSSW_9_3_3
 cd CMSSW_9_3_3/src
-cmsenv
+eval `scramv1 runtime -sh`
 cd -
 
 g++ -g -O3 `root-config --glibs --cflags` fittingClass/mipFitsSiPM.C -o mipFitsSiPM
