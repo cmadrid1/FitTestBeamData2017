a40e6111bb892abda4d6e0120e28d863c294bf44
Christopher_Madrid <Chris_Madrid@baylor.edu>
2018-01-20 14:26:46 +0100

    Fixing the cms env setup

--- travisScript.sh
+++ travisScript.sh
@@ -4,7 +4,7 @@ cd $1
 #source /cvmfs/sft.cern.ch/lcg/views/LCG_89/x86_64-slc6-gcc62-opt/setup.sh
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 
-cmsrel CMSSW_9_3_3
+scramv1 project CMSSW CMSSW_9_3_3
 cd CMSSW_9_3_3/src
 cmsenv
 cd -
