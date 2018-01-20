9c83efb0c3d4aeac6cfc17b77e3e91087c11b03b
Christopher_Madrid <Chris_Madrid@baylor.edu>
2018-01-20 14:21:23 +0100

    setting up a cmssw env. (overkill)

--- travisScript.sh
+++ travisScript.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-cd $1 
-source /cvmfs/sft.cern.ch/lcg/views/LCG_89/x86_64-slc6-gcc62-opt/setup.sh
+cd $1
+#source /cvmfs/sft.cern.ch/lcg/views/LCG_89/x86_64-slc6-gcc62-opt/setup.sh
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+
+cmsrel CMSSW_9_3_3
+cd CMSSW_9_3_3/src
+cmsenv
+cd -
 
 g++ -g -O3 `root-config --glibs --cflags` fittingClass/mipFitsSiPM.C -o mipFitsSiPM
